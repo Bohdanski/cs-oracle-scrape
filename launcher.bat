f7a7348f0f438af5e4cde10057c1589d19213a4a
Bohdan Tkachenko <bohdantk@buffalo.edu>
2020-05-05 16:22:22 -0400

    - launcher.bat: Will now run from root directory instead of hardcoded path

--- launcher.bat
+++ launcher.bat
@@ -1,10 +1,6 @@
-:: # Change directory here to match file location:
-
-cd C:\Users\tubxt2p\Documents\Python\python-projects\cs-oracle-scrape\
-
-
+:: Assign current working directory (root directory) to variable X
+set X=%CD%
 
 :: # Change file name here
-
 :: python tests.py
 python cs_oracle_scrape.py
